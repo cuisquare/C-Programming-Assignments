6df6c6f9703aa22e16e7594afdf24ac3125b620c
cuisquare <cuisquare@gmail.com>
2018-06-21 15:42:16 +0100

    Comment meaning of Order in Types.h.

--- DoubleLinkedListAssignment/Types.h
+++ DoubleLinkedListAssignment/Types.h
@@ -1,9 +1,11 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-//enum defining direction that list goes when moving forward
-// asc means ascending value list going forward
-// desc means descending value list going forward
+//enum defining direction that list goes when accessing next element
+// asc means first element is smallest, last is greatest, next is ascending
+// desc means first element is greatest, last is smallest and next is descending
+// in memory the list will however be stored in ascending order going fwd and this will remain unchanged
+// only used effectively in print functions
 enum Order {asc, desc};
 
 //Struct type for a double linked list element. 
